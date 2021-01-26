d702a989f43c7184872886537146f5c14019706c
Jacob Tepperman <jtepp@icloud.com>
2021-01-25 23:28:10 -0500

    Initial Commit

---
+++ iCons/iConsApp.swift
@@ -0,0 +1,17 @@
+//
+//  iConsApp.swift
+//  iCons
+//
+//  Created by Jacob Tepperman on 2021-01-25.
+//
+
+import SwiftUI
+
+@main
+struct iConsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
