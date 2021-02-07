9cc2bc2d8494c7d4ad2357a81f9b401e8b32bf32
Jacob Tepperman <jtepp@icloud.com>
2021-02-06 19:33:18 -0500

    initialized dropdownView

---
+++ iCons/DropdownView.swift
@@ -0,0 +1,20 @@
+//
+//  DropdownView.swift
+//  Pods
+//
+//  Created by Jacob Tepperman on 2021-02-06.
+//
+
+import SwiftUI
+
+struct DropdownView: View {
+    var body: some View {
+        Text("d")
+    }
+}
+
+struct DropdownView_Previews: PreviewProvider {
+    static var previews: some View {
+        DropdownView()
+    }
+}
